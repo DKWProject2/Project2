ae8263608f22e20111b28bdc8a31bf355e2d6ff4
pateldarsh14 <43825079+pateldarsh14@users.noreply.github.com>
2019-11-06 00:12:17 -0600

    added javascript for nav

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* for side nav that pops out */
+function openNav() {
+  document.getElementById("sidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("sidenav").style.width = "0";
+}
