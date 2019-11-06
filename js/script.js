0ac2030359526fc7da328c15155b1f4f25c8c2ac
khaque1 <khaque1@users.noreply.github.com>
2019-11-06 16:37:35 -0600

    adding js in personal repository

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/* for side nav that pops out */
+function openNav() {
+  document.getElementById("sidenav").style.width = "200px";
+}
+
+function closeNav() {
+  document.getElementById("sidenav").style.width = "0";
+}
+
+// Get the elements with class="column"
+<<<<<<< HEAD
+var elements = document.getElementsByClassName("column");
+=======
+var elements = document.getElementsByClassName("column");
+>>>>>>> 8ac3581842d93502b5d15cf25d7cf7e6af834c37
